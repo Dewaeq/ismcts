c40156ed328a3ab420431d5abe91baea40bae7de
dewaeq <dewaeq@protonmail.com>
2025-03-07 23:27:59 +0100

    ismcts algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod action_list;
+pub(crate) mod edge;
+pub(crate) mod node;
+pub mod searcher;
+pub mod state;
+pub(crate) mod tree;
